e0a933b8e263b7ae89875f3e6fcc287e55df143b
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-19 11:31:32 -0700

    Include middleware to allow CORS requests

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -9,6 +9,12 @@ module.exports = function (app, express) {
   var triviaRouter = express.Router();
   var gameRouter = express.Router();
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
